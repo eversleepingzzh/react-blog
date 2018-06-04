1f45cac0da3454f47f269f0f09ee52a5263ddf5e
zzh49358 <zhangzhihao@chebada.com>
2018-06-04 16:10:24 +0800

    webpack4 configure

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+import './css/index.less'
+
+import App from './js/App.js';
+
+ReactDom.render(
+    <App/>,
+    document.getElementById("root")
+);
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    //entry为入口,webpack从这里开始编译
+    entry: [
+        "babel-polyfill",
+        path.join(__dirname, './src/index.js')
+    ],
+    //output为输出 path代表路径 filename代表文件名称
+    output: {
+        path: path.join(__dirname, './bundle'),
+        filename: 'bundle.[hash:8].js',
+        chunkFilename: '[name].[chunkhash:8].js'
+    },
+    //module是配置所有模块要经过什么处理
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: ['babel-loader'],
+                include: path.join(__dirname , 'src'),
+                exclude: /node_modules/
+            },{
+                test: /\.less$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ['css-loader?modules&localIdentName=[local]-[hash:base64:5]', 'postcss-loader', 'less-loader']
+                })
+            },{
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: ['postcss-loader', 'css-loader']
+                })
+            },{
+                test: /\.(png|jpg|gif)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            filename: "index.html",  //打包后的文件名
+            template: path.join(__dirname , "./src/index.html")  //要打包文件的路径
+        }),
+        new ExtractTextPlugin({
+            filename: 'index.[hash:8].css'
+        }),
+        new CleanWebpackPlugin(['bundle'])
+    ],
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'common',
+                    chunks: 'all'
+                }
+            }
+        }
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'bundle'),  //启动路径
+        host:'localhost',  //域名
+        port: 8018,  //端口号
+    },
+    mode: 'development',
+    devtool: ''
+};
