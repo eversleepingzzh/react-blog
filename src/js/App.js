93b8306de0ffe0a9ec1f40d28b3df6611c6233aa
zzh49358 <zhangzhihao@chebada.com>
2018-06-05 16:53:57 +0800

    add blogIntro

--- src/js/App.js
+++ src/js/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-import SideBar from './component/sidebar.js'
+import SideBar from './component/sidebar.js';
+import BlogIntro from './component/blogIntro.js';
 import style from '../css/App.css';
 
 export default class App extends React.Component {
@@ -8,6 +9,9 @@ export default class App extends React.Component {
         return (
             <div className="blog-container">
                 <SideBar />
+                <div className="blog-content">
+                    <BlogIntro />
+                </div>
             </div>
         )
     }
---
+++ src/js/component/blogIntro.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import '../../css/blogIntro.css';
+
+
+export default class BlogIntro extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            article:[],
+        }
+    }
+
+    componentDidMount() {
+        this.setState({
+            article:[1,2,3,4,5,6]
+        },() => {
+            console.log(this.state.article);
+        });
+    }
+    render() {
+        var article = this.state.article
+        return (
+            <div className='content-container'>
+                {
+                    article.map((index) => {
+                        return(
+                            <div className="content-article" key={index}>
+                                <div className="article-header">
+                                    <h1>深入理解react</h1>
+                                    <a>2017-7-17</a>
+                                </div>
+                                <div className="article-entry">
+                                    <p>目前mac下安装node环境有点麻烦，折腾了一段时间，终于安装成功，下面是一些记录，方便有同样需要的人<br/>
+                                    <a className="article-more-a" href="">more &gt;&gt;</a>
+                                    </p>
+                                </div>
+                                <div className='article-intro'>
+
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+    }
+}
