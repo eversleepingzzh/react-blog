7b5f0cb184f33afbc2a35cfb1ea6f471cca7ef7f
zzh49358 <zhangzhihao@chebada.com>
2018-06-05 17:34:33 +0800

    add rough router

--- src/js/App.js
+++ src/js/App.js
@@ -5,7 +5,42 @@ import BlogIntro from './component/blogIntro.js';
 import style from '../css/App.css';
 
 export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            route:window.location.hash
+        }
+    }
+
+    componentDidMount() {
+        var self = this;
+
+        setTimeout(() => {
+            window.addEventListener('hashchange',function() {
+                var route = window.location.hash.substr(1)
+                self.setState({
+                    route:route
+                },() => {
+                    console.log(self.state.route);
+                })
+            })
+        },0)
+    }
     render () {
+        var child;
+        switch (this.state.route) {
+            case '':
+                Child = BlogIntro;
+                break;
+            case 'Add':
+                Child = Add;
+                break;
+            case 'MineLostlist':
+                Child = MineLostlist;
+                break;
+            default:
+                Child = null;
+        }
         return (
             <div className="blog-container">
                 <SideBar />
